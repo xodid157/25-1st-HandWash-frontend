62cef9448895131a199e71885989cc9d766a7afd
yoooon0w0 <87513016+yoooon0w0@users.noreply.github.com>
2021-10-07 15:30:04 +0900

    Refactor: 레이아웃 정리중

--- src/pages/ItemList/Itemlist.js
+++ src/pages/ItemList/Itemlist.js
@@ -4,19 +4,40 @@ import './Itemlist.scss';
 
 class Item extends React.Component {
   render() {
-    return <></>;
+    const { name, price, img } = this.props;
+    return (
+      <li className="item">
+        <div className="itemImg">
+          <Link to="#">
+            <img src={img} alt={name} />
+          </Link>
+          <div className="imgInPrice">
+            <p>{price}</p>
+          </div>
+          <i class="fas fa-heart fa-2x"></i>
+        </div>
+        <div className="itemText">
+          <div className="ex">Conscious</div>
+          <div className="itemName">
+            <Link to="#">{name}</Link>
+          </div>
+          <div className="itemprice">{price}</div>
+          <div className="itemColor">
+            <Link to="#">
+              <div></div>
+            </Link>
+            <Link to="#">
+              <div></div>
+            </Link>
+          </div>
+          <div className="newItem">신제품</div>
+        </div>
+      </li>
+    );
   }
 }
 
 class ItemList extends React.Component {
-  //List =>(id,img,name,price,color,new,)
-  // constructor(props){
-  //   super(props)
-  //   this.state={
-  //     itemList,
-  //   }
-  // }
-
   render() {
     return (
       <div className="list">
@@ -30,106 +51,92 @@ class ItemList extends React.Component {
               영캐주얼부터 뷰티제품까지 다양하게 준비되어 있으니 지금 둘러보세요
             </p>
           </div>
-          <div className="navFilter">
-            <ul class="navi">
-              <li>
-                <a href="#">정렬 기준</a>
-                <ul>
-                  <li>
-                    <a href="#">추천</a>
-                  </li>
-                  <li>
-                    <a href="#">최신순</a>
-                  </li>
-                  <li>
-                    <a href="#">낮은 가격</a>
-                  </li>
-                  <li>
-                    <a href="#">높은 가격</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">사이즈</a>
-                <ul>
-                  <li>
-                    <a href="#">XS</a>
-                  </li>
-                  <li>
-                    <a href="#">S</a>
-                  </li>
-                  <li>
-                    <a href="#">M</a>
-                  </li>
-                  <li>
-                    <a href="#">L</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#">컬러</a>
-                <ul>
-                  <li>
-                    <a href="#">RED</a>
-                  </li>
-                  <li>
-                    <a href="#">BLUE</a>
-                  </li>
-                  <li>
-                    <a href="#">BLACK</a>
-                  </li>
-                  <li>
-                    <a href="#">PPPPPPP</a>
-                  </li>
-                </ul>
-              </li>
-            </ul>
-            <div className="navFilterRight">
-              <div className="itemListNum">1234아이템</div>
-              <div>
-                <a href="#">모델</a>
-              </div>
-              <div>
-                <a href="#">제품</a>
-              </div>
-              <div>
-                <a href="#">
-                  <i class="far fa-square"></i>
-                </a>
-              </div>
-              <div>
-                <a href="#">
-                  <i class="fas fa-th-large"></i>
-                </a>
+          <div className="bigfilter">
+            <div className="navFilter">
+              <ul class="navi">
+                <li>
+                  <Link to="#">정렬 기준</Link>
+                  <ul>
+                    <li>
+                      <Link to="#">추천</Link>
+                    </li>
+                    <li>
+                      <Link to="#">최신순</Link>
+                    </li>
+                    <li>
+                      <Link to="#">낮은 가격</Link>
+                    </li>
+                    <li>
+                      <Link to="#">높은 가격</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <Link to="#">사이즈</Link>
+                  <ul>
+                    <li>
+                      <Link to="#">XS</Link>
+                    </li>
+                    <li>
+                      <Link to="#">S</Link>
+                    </li>
+                    <li>
+                      <Link to="#">M</Link>
+                    </li>
+                    <li>
+                      <Link to="#">L</Link>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <Link to="#">컬러</Link>
+                  <ul>
+                    <li>
+                      <Link to="#">RED</Link>
+                    </li>
+                    <li>
+                      <Link to="#">BLUE</Link>
+                    </li>
+                    <li>
+                      <Link to="#">BLACK</Link>
+                    </li>
+                    <li>
+                      <Link to="#">PPPPPPP</Link>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <div className="navFilterRight">
+                <div className="itemListNum">1234아이템</div>
+                <div>
+                  <a href="#">모델</a>
+                </div>
+                <div>
+                  <a href="#">제품</a>
+                </div>
+                <div>
+                  <a href="#">
+                    <i class="far fa-square"></i>
+                  </a>
+                </div>
+                <div>
+                  <a href="#">
+                    <i class="fas fa-th-large"></i>
+                  </a>
+                </div>
               </div>
             </div>
+            <ul className="items">
+              {LIST.map(item => (
+                <Item
+                  key={item.id}
+                  name={item.name}
+                  price={item.price}
+                  img={item.img}
+                />
+              ))}
+            </ul>
           </div>
-          <ul className="items">
-            <li className="item">
-              <div className="itemImg">
-                <Link to="#">
-                  <img src="images/1111.jpeg" alt="itemimage" />
-                </Link>
-                <div className="imgInPrice">
-                  <p>W77,000</p>
-                </div>
-                <i class="fas fa-heart fa-2x"></i>
-              </div>
-              <div className="itemText">
-                <div className="ex">Conscious</div>
-                <div className="itemName">
-                  <Link to="#">리브니트 울 블랜드 드레스</Link>
-                </div>
-                <div className="itemprice">W77,000</div>
-                <div className="itemColor">
-                  <Link to="#">x</Link>
-                  <Link to="#">x</Link>
-                  <Link to="#">x</Link>
-                </div>
-                <div className="newItem">신제품</div>
-              </div>
-            </li>
-          </ul>
         </div>
       </div>
     );
@@ -137,6 +144,53 @@ class ItemList extends React.Component {
 }
 export default ItemList;
 
-// const List = [
-//   {id: , img:'' , name:,price:,color:,isnew:,}
-// ]
+const LIST = [
+  {
+    id: 1,
+    name: '리브니트 울 블랜드 드레스',
+    price: 'W77,000',
+    img: 'images/1111.jpeg',
+  },
+  {
+    id: 1,
+    name: '리브니트 울 블랜드 드레스',
+    price: 'W77,000',
+    img: 'images/1111.jpeg',
+  },
+  {
+    id: 1,
+    name: '리브니트 울 블랜드 드레스',
+    price: 'W77,000',
+    img: 'images/1111.jpeg',
+  },
+  {
+    id: 1,
+    name: '리브니트 울 블랜드 드레스',
+    price: 'W77,000',
+    img: 'images/1111.jpeg',
+  },
+  {
+    id: 1,
+    name: '리브니트 울 블랜드 드레스',
+    price: 'W77,000',
+    img: 'images/1111.jpeg',
+  },
+  {
+    id: 1,
+    name: '리브니트 울 블랜드 드레스',
+    price: 'W77,000',
+    img: 'images/1111.jpeg',
+  },
+  {
+    id: 1,
+    name: '리브니트 울 블랜드 드레스',
+    price: 'W77,000',
+    img: 'images/1111.jpeg',
+  },
+  {
+    id: 1,
+    name: '리브니트 울 블랜드 드레스',
+    price: 'W77,000',
+    img: 'images/1111.jpeg',
+  },
+];
