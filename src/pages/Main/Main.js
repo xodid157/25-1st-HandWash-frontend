70e873d50963a2cbd3ef33fc7e72bb1682e5a36f
Jetom88 <shinhro3o@gmail.com>
2021-10-07 16:07:45 +0900

    Add: main section 추가중

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -6,7 +6,7 @@ class Main extends React.Component {
     return (
       <main>
         <div className="trendContent">
-          <h3>최신 트렌드</h3>
+          <h3 className="trendTitle">최신 트렌드</h3>
 
           <div className="trendImgContent">
             <i class="fas fa-arrow-left"></i>
@@ -96,8 +96,53 @@ class Main extends React.Component {
           </div>
 
           <section>
-            <div className="CampaignTeaser">
-              <img src={'/images/main_img.jpeg'} alt="" />
+            <div className="campaignTeaser">
+              <div className="campaignContent">
+                <img src={'/images/main_img.jpeg'} alt="" />
+                <div>
+                  <h3>
+                    가을 머스트 <b>&#x002D;</b> 해브
+                  </h3>
+                  <p>
+                    멋스러운 니트, 재킷, 가디건 등 시즌 인기 아이템을 지금
+                    만나보세요.
+                  </p>
+
+                  <a href="">쇼핑하기</a>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="campaignTeaser">
+              <div className="campaignContent">
+                <img src={'/images/main_img.jpeg'} alt="" />
+                <div>
+                  <h3>HandWash</h3>
+                  <p>
+                    Kim Minho / Seo Donghyeok / Shin hyeri <br />
+                    Lee Dabin / Jeon Taeyang / Jo Yunhui
+                  </p>
+
+                  <a href="">지금 쇼핑하기</a>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section>
+            <div className="campaignTeaser">
+              <div className="campaignContent">
+                <img src={'/images/main_img.jpeg'} alt="" />
+                <div>
+                  <h3>HandWash</h3>
+                  <p>
+                    Kim Minho / Seo Donghyeok / Shin hyeri <br />
+                    Lee Dabin / Jeon Taeyang / Jo Yunhui
+                  </p>
+
+                  <a href="">지금 쇼핑하기</a>
+                </div>
+              </div>
             </div>
           </section>
         </div>
