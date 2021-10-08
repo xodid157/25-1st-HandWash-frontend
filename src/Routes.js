a878b7852f3daa9780b3af859a2a1ca15ddbf35b
Jetom88 <shinhro3o@gmail.com>
2021-10-08 10:47:43 +0900

    Add: Routes에 footer 추가

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,16 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import Main from './pages/Main/Main';
 import Nav from './components/Nav/Nav';
+import Footer from './components/footer/Footer';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
-          <Route exact path="/main" component={Main} />
           <Route exact path="/nav" component={Nav} />
+          <Route exact path="/main" component={Main} />
+          <Route exact path="/footer" component={Footer} />
         </Switch>
       </Router>
     );
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,7 +2,15 @@ import React, { Component } from 'react';
 
 class Footer extends Component {
   render() {
-    return <div>footer!</div>;
+    return (
+      <footer>
+        <div className="footerIcons">
+          <a href="">
+            <i class="fas fa-sun"></i>
+          </a>
+        </div>
+      </footer>
+    );
   }
 }
 
