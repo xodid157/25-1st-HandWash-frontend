b26b4f3d253949113d25dc7bf8f4cb65594ea037
Jetom88 <shinhro3o@gmail.com>
2021-10-10 15:31:05 +0900

    Modify: main, nav 파일 삭제

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Nav.scss';
 
 class Nav extends React.Component {
   render() {
--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './Main.scss';
 
 class Main extends React.Component {
   render() {
