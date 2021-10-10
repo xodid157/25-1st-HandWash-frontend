42ad24a3c854b68a7a8fe1ccefe2924ffb3edaf9
yoooon0w0 <87513016+yoooon0w0@users.noreply.github.com>
2021-10-10 19:35:13 +0900

    Fix:버그수정중

--- src/pages/ItemList/Itemlist.js
+++ src/pages/ItemList/Itemlist.js
@@ -55,7 +55,7 @@ class ItemList extends React.Component {
           </div>
           <div className="content">
             <p>
-              새로운 필터 기능을 사용해서 당신에게 딱 맞는 사이즈를 찾아보세요!
+              1새로운 필터 기능을 사용해서 당신에게 딱 맞는 사이즈를 찾아보세요!
               영캐주얼부터 뷰티제품까지 다양하게 준비되어 있으니 지금 둘러보세요
             </p>
           </div>
