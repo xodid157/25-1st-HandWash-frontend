f12b63a9fdbb3b18e4f4cd057d8e8c808faef37b
Jetom88 <shinhro3o@gmail.com>
2021-10-11 18:21:25 +0900

    Add: login 컴포넌트 생성

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { SERVICES_LIST, USER_MENU } from './data';
+import Login from '../Login/Login';
+import { SERVICES_LIST, MAIN_MENU_LIST, USER_MENU } from './data';
 import './Nav.scss';
 
 class Nav extends React.Component {
@@ -13,9 +14,17 @@ class Nav extends React.Component {
   }
 
   componentDidMount() {
-    fetch('http://172.30.1.12:8000/products/categories', {
-      method: 'GET',
-    })
+    // fetch('http://172.30.1.12:8000/products/categories', {
+    //   method: 'GET',
+    // })
+    //   .then(res => res.json())
+    //   .then(category => {
+    //     this.setState({
+    //       categoryList: category,
+    //     });
+    //   });
+
+    fetch('data/navListData.json')
       .then(res => res.json())
       .then(category => {
         this.setState({
@@ -26,10 +35,12 @@ class Nav extends React.Component {
 
   render() {
     const { categoryList } = this.state;
+    console.log(categoryList.category_list[0].main_category?.[0].name);
+
     return (
       <nav className="navContainer">
         <div className="menuServices">
-          <ul>
+          <ul class="menuServicesList">
             {SERVICES_LIST.map((el, idx) => (
               <li key={idx}>{el}</li>
             ))}
@@ -42,6 +53,8 @@ class Nav extends React.Component {
                 <span>{userMenu.txt}</span>
               </li>
             ))}
+
+            <Login />
           </ul>
         </div>
 
@@ -51,9 +64,19 @@ class Nav extends React.Component {
 
         <div>
           <ul className="menu">
-            {/* {categoryList.map((category, idx) => {
-              return <li key={idx}>{category.category_list.name}</li>;
-            })} */}
+            {categoryList.category_list.map((category, idx) => {
+              return <li key={idx}>{category.name}</li>;
+            })}
+
+            {MAIN_MENU_LIST.map((mainMenu, idx) => {
+              return <li key={idx}>{mainMenu}</li>;
+            })}
+          </ul>
+
+          <ul className="subMenu">
+            {categoryList.category_list.main_category?.map((subMenu, idx) => {
+              return <li key={idx}>{subMenu.name}</li>;
+            })}
           </ul>
         </div>
 
--- src/components/Nav/data.js
+++ src/components/Nav/data.js
@@ -5,6 +5,14 @@ export const SERVICES_LIST = [
   <i className="fas fa-ellipsis-h" />,
 ];
 
+export const MAIN_MENU_LIST = [
+  'Divided',
+  '신생아/유아',
+  'H&W HOME',
+  'Sale',
+  '지속가능성',
+];
+
 export const USER_MENU = [
   { id: 1, type: 'fa-user', txt: '로그인' },
   { id: 2, type: 'fa-heart', txt: '즐겨찾기' },
