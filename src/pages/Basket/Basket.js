b90540036b2e4ffcfdfccf5b091067ce52ec2999
gur0601 <80812195+gur0601@users.noreply.github.com>
2021-10-13 18:26:23 +0900

    WIP

--- src/pages/Basket/Basket.js
+++ src/pages/Basket/Basket.js
@@ -3,43 +3,48 @@ import './Basket.scss';
 class Basket extends React.Component {
   render() {
     return (
-      <>
-        <header>
-          <p className="basketTitle">쇼핑백</p>
-        </header>
+      <div className="cart">
+        <p className="basketTitle">쇼핑백</p>
+
         <div className="basketContainer">
           <main className="shoppingBasket">
             <ul className="basketList">
-              <li>
+              <li className="addItem">
                 <img
-                  src="https://images.unsplash.com/photo-1521572163474-6864f9cf17ab?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80
+                  src="https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=795&q=80
 "
-                  alt="반팔"
+                  alt="outer"
+                  className="itemImg"
                 ></img>
-                <div className="itemName">
-                  <span>A라인 쇼트 드레스</span>
-                  <span>₩ 29,990</span>
+                <span className="itemInfo">
+                  <span className="itemTag">
+                    <span>A라인 쇼트 드레스</span>
+                    <span className="price">₩ 29,990</span>
+                  </span>
                   {/* 나중에 중괄호 한다음 데이터 값을 받을거임 */}
-                </div>
-                <i className="far fa-trash-alt"></i>
-                <ul className="itemInfo">
-                  <li>
-                    <span>상품 번호:</span>
-                    <span>0991090004</span>
-                  </li>
-                  <li>
-                    <span>사이즈:</span>
-                    <span>42</span>
-                  </li>
-                  <li>
-                    <span>컬러:</span>
-                    <span>오렌지/화이트 플로럴</span>
-                  </li>
-                  <li>
-                    <span>합계:</span>
-                    <span>₩ 29,990</span>
-                  </li>
-                </ul>
+
+                  <i className="far fa-trash-alt"></i>
+                  <span>
+                    <ul className="itemInfo">
+                      <li>
+                        <span>상품 번호:</span>
+                        <span>0991090004</span>
+                      </li>
+                      <li>
+                        <span>사이즈:</span>
+                        <span>42</span>
+                      </li>
+                      <li>
+                        <span>컬러:</span>
+                        <span>오렌지/화이트 플로럴</span>
+                      </li>
+                      <li>
+                        <span>합계:</span>
+                        <span>₩ 29,990</span>
+                      </li>
+                    </ul>
+                  </span>
+                </span>
                 <button>
                   <i className="far fa-heart"></i>
                 </button>
@@ -53,7 +58,7 @@ class Basket extends React.Component {
           </main>
           <div className="cartSide">할인 코드 추가</div>
         </div>
-      </>
+      </div>
     );
   }
 }
