bed8a957a6fcde51815dfdb182fdbdf989e15ffd
gur0601 <80812195+gur0601@users.noreply.github.com>
2021-10-13 20:01:18 +0900

    WIP

--- src/pages/Basket/Basket.js
+++ src/pages/Basket/Basket.js
@@ -10,6 +10,7 @@ class Basket extends React.Component {
           <main className="shoppingBasket">
             <ul className="basketList">
               <li className="addItem">
+                {/* <i className="far fa-trash-alt"></i> */}
                 <img
                   src="https://images.unsplash.com/photo-1487222477894-8943e31ef7b2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=795&q=80
 "
@@ -18,41 +19,39 @@ class Basket extends React.Component {
                 ></img>
                 <span className="itemInfo">
                   <span className="itemTag">
-                    <span>A라인 쇼트 드레스</span>
-                    <span className="price">₩ 29,990</span>
+                    <p>A라인 쇼트 드레스</p>
+                    <p className="price">₩ 29,990</p>
                   </span>
                   {/* 나중에 중괄호 한다음 데이터 값을 받을거임 */}
 
-                  <i className="far fa-trash-alt"></i>
-                  <span>
-                    <ul className="itemInfo">
-                      <li>
-                        <span>상품 번호:</span>
-                        <span>0991090004</span>
-                      </li>
-                      <li>
-                        <span>사이즈:</span>
-                        <span>42</span>
-                      </li>
-                      <li>
-                        <span>컬러:</span>
-                        <span>오렌지/화이트 플로럴</span>
-                      </li>
-                      <li>
-                        <span>합계:</span>
-                        <span>₩ 29,990</span>
-                      </li>
-                    </ul>
-                  </span>
+                  <ul className="itemInfoList">
+                    <li>
+                      <span>상품 번호:</span>
+                      <span>0991090004</span>
+                    </li>
+                    <li>
+                      <span>사이즈:</span>
+                      <span>42</span>
+                    </li>
+                    <li>
+                      <span>컬러:</span>
+                      <span>오렌지/화이트 플로럴</span>
+                    </li>
+                    <li>
+                      <span>합계:</span>
+                      <span>₩ 29,990</span>
+                    </li>
+                  </ul>
+
+                  <button className="addHarth">
+                    <i className="far fa-heart"></i>
+                  </button>
+                  <select className="count" name="count">
+                    <option>1</option>
+                    <option>2</option>
+                    <option>3</option>
+                  </select>
                 </span>
-                <button>
-                  <i className="far fa-heart"></i>
-                </button>
-                <select className="count" name="count">
-                  <option>1</option>
-                  <option>2</option>
-                  <option>3</option>
-                </select>
               </li>
             </ul>
           </main>
